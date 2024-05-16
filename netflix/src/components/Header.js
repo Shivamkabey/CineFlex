c7c13bb14381e3762afcefc5247579a702554fb1
Shivam-kabey <Shivamkabey@gmail.com>
2024-05-16 19:00:31 +0530

    login ,signup created

--- netflix/src/components/Header.js
+++ netflix/src/components/Header.js
@@ -3,12 +3,12 @@ import { IoIosArrowDropdown } from "react-icons/io";
 
 const Header = () => {
   return (
-    <div className='absolute z-10 flex w-[100vw] items-centre justify-between px-6 bg-gradient-to-b from-black'>
+    <div className='absolute z-10 flex w-[100vw] items-center justify-between px-6 bg-gradient-to-b from-black'>
       <img className='w-56' src='https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png' alt="Netflix Logo" />
-      <div className='flex item-centre py-4'>
+      <div className='flex items-center py-4'>
       <IoIosArrowDropdown size="24px" color='white'/>
         <h1 className='text-lg font-medium text-white'>MernNet </h1>
-        <div className='ml-4 '>
+        <div className='ml-4'>
           <button className='bg-red-800 text-white px-4 py-2 '>Logout</button>
           <button className='bg-red-800 text-white px-4 py-2 ml-2 '>Search Movie</button>
         </div>
--- netflix/src/components/Login.js
+++ netflix/src/components/Login.js
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const Login = () => {
+  const [isLogin, setIsLogin] = useState(false);
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginhandler = () => {
+    setIsLogin(!isLogin)
+  }
+
+  const getInputData =(e)=>{
+    e.preventDefault();
+    console.log(fullname,email,password);
+  }
+
   return (
     <div>
-        <Header/>
-        <div className='absolute'>
-            <img className='w-[100vw] h-[100vh] bg-cover' src='https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/netflixteaser.png' alt='bg'/>
+      <Header />
+      <div className='absolute'>
+        <img className='w-[100vw] h-[100vh] bg-cover' src='https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/netflixteaser.png' alt='landing banner' />
+      </div>
+      <form onSubmit={getInputData} className='flex flex-col w-3/12 my-36 p-12 left-0 right-0 mx-auto items-center justify-center absolute rounded-md bg-black opacity-90' >
+        <h1 className='text-3xl text-white mb-5 font-bold'>{isLogin ? "Login" : "Signup"}</h1>
+        <div className='flex flex-col'>
+          {
+            !isLogin && <input value={fullname} onChange={(e)=>setFullname(e.target.value)} type='text' placeholder='Full Name' className='outline-none p-3 my-2 rounded-sm bg-gray-800 text-white' />
+          }
+          <input value={email} onChange={(e)=>setEmail(e.target.value)} type='email' placeholder='Email' className='outline-none p-3 my-2 rounded-sm bg-gray-800 text-white' />
+          <input value={password} onChange={(e)=>setPassword(e.target.value)} type='password' placeholder='Password' className='outline-none p-3 my-2 rounded-sm bg-gray-800 text-white' />
+          <button className='bg-red-600 mt-6 mb-2 p-3 text-white rounded-sm  font-medium'>{isLogin ? "Login":"Signup"}</button>
+          <p className='text-white'>{isLogin ? "New to Netflix?" : "Already have an account?"}<span onClick={loginhandler} className='ml-1 text-blue-900 font-medium cursor-pointer'>{isLogin ? "Signup" : "Login"}</span></p>
         </div>
-        <form className='flex flex-col w-3/12 my-36 left-0 right-0 mx-auto items-center justify-center absolute bg-red-800' >
-          <div className='flex flex-col'>
-            <input type='text' placeholder='Full Name'/>
-            <input type='email' placeholder='Email'/>
-            <input type='text' placeholder='Password'/>
-          </div>
-        </form>
+      </form>
     </div>
   )
 }
