206aeff35ae86da13e22b043ad2aa8a8ded8cf0c
Shivam-kabey <Shivamkabey@gmail.com>
2024-05-16 13:19:15 +0530

    login page

--- netflix/src/components/Header.js
+++ netflix/src/components/Header.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { IoIosArrowDropdown } from "react-icons/io";
 
 export const Header = () => {
   return (
-    <div>
-        <img className='w-56' src='https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png' alt ="Netflix Logo"/>
-        <div>
-          <h1>MernNet</h1>
+    <div className='absolute z-10 flex w-[100vw] items-centre justify-between px-6 bg-gradient-to-b from-black'>
+      <img className='w-56' src='https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png' alt="Netflix Logo" />
+      <div className='flex item-centre py-4'>
+      <IoIosArrowDropdown size="24px" color='white'/>
+        <h1 className='text-lg font-medium text-white'>MernNet </h1>
+        <div className='ml-4 '>
+          <button className='bg-red-800 text-white px-4 py-2 '>Logout</button>
+          <button className='bg-red-800 text-white px-4 py-2 ml-2 '>Search Movie</button>
         </div>
+      </div>
     </div>
   )
 }
--- netflix/src/components/Login.js
+++ netflix/src/components/Login.js
@@ -5,9 +5,16 @@ export const Login = () => {
   return (
     <div>
         <Header/>
-        <div>
-            Login
+        <div className='absolute'>
+            <img className='w-[100vw] h-[100vh] bg-cover' src='https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/netflixteaser.png' alt='bg'/>
         </div>
+        <form className='flex flex-col w-3/12 my-36 left-0 right-0 mx-auto items-center justify-center absolute bg-red-800' >
+          <div className='flex flex-col'>
+            <input type='text' placeholder='Full Name'/>
+            <input type='email' placeholder='Email'/>
+            <input type='text' placeholder='Password'/>
+          </div>
+        </form>
     </div>
   )
 }
